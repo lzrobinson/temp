4afc5c4ab06e112e8210af4e5a89e711169fb1f4
Bhavleen Kaur <bhavleenmehar1301@gmail.com>
2022-09-09 10:48:11 +1000

    added dependencies for sqflite and basic code for review db

---
+++ lets_study_kitti/lib/ReviewDb.dart
@@ -0,0 +1,19 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package/path_provider/path_provider.dart';
+
+class ReviewDb{
+
+  setDatabase() async{
+
+    var directory = await getApplicationDocumentDirectory();
+    var path = join(directory.path, 'db_review_sqflite');
+    var database = await openDatabase(path, version: 1, onCreate: _onCreatingDatabase);
+    return database;
+  }
+
+  _onCreatingDatabase(Database database) async{
+    await database.execute(
+      "CREATE TABLE reviews(ReviewID INTEGER PRIMARY KEY, SubjectCode INTEGER, UserID INTEGER, Lecturer TEXT, Compulsory INTEGER, ExamWeightage INTEGER, Period TEXT, TeachingQuality INTEGER, Difficulty INTEGER, Interesting INTEGER, Review TEXT, Recommended INTEGER)");
+  }
+}
