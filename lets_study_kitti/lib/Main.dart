e940153fd91b1df7fa64d3e5601276b0dd75ac75
senturner <stturner@student.unimelb.edu.au>
2022-09-17 15:50:07 +1000

    updated main

--- lets_study_kitti/lib/Main.dart
+++ lets_study_kitti/lib/Main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:lets_study_kitti/Screens/ReviewFormPage.dart' show ReviewFormPage;
+import 'package:lets_study_kitti/Screens/ReviewFormPage.dart'
+    show ReviewFormPage;
 import 'package:lets_study_kitti/Screens/SubjectPage.dart' show SubjectPage;
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:intl/intl.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-Future main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  runApp(MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -30,14 +32,13 @@ class MyApp extends StatelessWidget {
       ],
       supportedLocales: FormBuilderLocalizations.delegate.supportedLocales,
       home: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background.png'),
-            fit: BoxFit.cover,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: ReviewFormPage()
-      ),
+          child: ReviewFormPage()),
     );
   }
 }
